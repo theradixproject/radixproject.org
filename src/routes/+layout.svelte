537e2e2d9a79513ed34fc86db0c2d06825865569
Nate Sales <nate@natesales.net>
2022-11-13 23:06:57 -0500

    feat: extract layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,21 @@
+<script>
+    import Navbar from "../components/Navbar.svelte";
+    import Footer from "../components/Footer.svelte";
+</script>
+
+<main>
+    <Navbar/>
+    <slot></slot>
+    <Footer/>
+</main>
+
+<style>
+    main {
+        min-height: 100vh;
+        margin: 0;
+        color: white;
+        background-color: black !important;
+        display: flex;
+        flex-direction: column;
+    }
+</style>
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,10 @@
+<main>
+    <h1 class="heading-title">About</h1>
+    <p>This is a test</p>
+</main>
+
+<style>
+    h1 {
+        font-size: 2em;
+    }
+</style>
