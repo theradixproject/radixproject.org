5a6dc637104ce6e085397c26d92c2a80f8fa4516
Nate Sales <nate@natesales.net>
2022-11-14 23:35:20 -0500

    reset gh pages

--- _app/immutable/chunks/10-85367c1e.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/terms/_page.svelte-bfa2c575.js";export{t as component};
--- _app/immutable/chunks/2-bdaaf900.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-460987d4.js";export{t as component};
