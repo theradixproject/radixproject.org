f4f1838e2f45a2a885c50a7e3d95b8ddb10c2721
Nate Sales <nate@natesales.net>
2022-11-15 00:01:00 -0500

    feat: add avif profiles

--- src/components/Img.svelte
+++ src/components/Img.svelte
@@ -3,15 +3,16 @@
 
     export let src, alt
     export let themed = false
+    export let round = false
 </script>
 
 <picture>
     {#if themed}
         <source type="image/avif" srcset="/{src}_{$light ? 'light' : 'dark'}.avif">
-        <img alt={alt} src="/{src}_{$light ? 'light' : 'dark'}.png">
+        <img class:round alt={alt} src="/{src}_{$light ? 'light' : 'dark'}.png">
     {:else}
         <source type="image/avif" srcset="/{src}.avif">
-        <img alt={alt} src="/{src}.png">
+        <img class:round alt={alt} src="/{src}.png">
     {/if}
 </picture>
 
@@ -19,5 +20,14 @@
     img, picture {
         width: inherit;
         height: inherit;
+    }
+
+
+    .round {
+        width: 175px;
+        height: 175px;
+        border-radius: 50%;
+        margin-bottom: 1rem;
+        border: 4px solid var(--color-purple);
     }
 </style>
--- src/routes/about/Person.svelte
+++ src/routes/about/Person.svelte
@@ -1,11 +1,13 @@
 <script>
+    import Img from "../../components/Img.svelte";
+
     export let title;
     export let name;
     export let user;
 </script>
 
 <main>
-    <img src="people/{user}.png" alt="{name} Profile Picture">
+   <Img round src="people/{user}" alt="{name} Profile Picture"/>
     <h3><span>{title}</span> {name}</h3>
     <a class="mono" href="mailto:{user}@radixproject.org">{user}@radixproject.org</a>
 </main>
@@ -18,14 +20,6 @@
         justify-content: center;
         text-align: center;
         padding: 1rem;
-    }
-
-    img {
-        width: 175px;
-        height: 175px;
-        border-radius: 50%;
-        margin-bottom: 1rem;
-        border: 4px solid var(--color-purple);
     }
 
     span {
