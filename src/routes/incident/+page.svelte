2e4e7dea6783f90553e1d47e4a2c30b19cc17b18
Nate Sales <nate@natesales.net>
2022-12-03 01:56:02 -0500

    feat: add incident page

---
+++ src/routes/incident/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+    import {onMount} from "svelte";
+    import SimplePage from "../../components/SimplePage.svelte";
+
+    onMount(() => {
+        document.location = "https://betteruptime.com/report/DTAvNkBAdfNqsxjFGaPcdjoH";
+    })
+</script>
+
+<SimplePage>
+    <div slot="title">
+        Loading<span class="dot">...</span>
+    </div>
+</SimplePage>
